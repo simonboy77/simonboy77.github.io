3bf0a975e8ff9c8498a3cc2b7ca6adc822234c8d
sj <simon@grimmaudio.com>
2025-02-13 23:04:44 +0100

    tryin some shit

--- src/test.js
+++ src/test.js
@@ -3,35 +3,35 @@ const yValues = [19, 23, 27, 29];
 const barColors = ["gray", "white","blue","purple"];
 
 var chart = new Chart("myChart", {
-  type: "bar",
-  data: {
-    labels: xValues,
-    datasets: [{
-      backgroundColor: barColors,
-      data: yValues
-    }],
-  },
-  options: {
-    scales: {
-      y: {
-      	min: 0,
-      }
-    },
-    plugins: {
-      title: {
-        display: true,
-        text: "Flatline Mags"
-      },
-      legend: {
-        display: false
-      }
-    }
-  }
+	type: "bar",
+	data: {
+		labels: xValues,
+		datasets: [{
+		backgroundColor: barColors,
+		data: yValues
+		}],
+	},
+	options: {
+		scales: {
+			y: {
+				min: 0,
+			}
+		},
+		plugins: {
+			title: {
+				display: true,
+				text: "Flatline Mags"
+				},
+			legend: {
+				display: false
+			}
+		}
+	}
 });
 
 var slider = document.getElementById("magTierCount");
 
 slider.oninput = function() {
-  chart.data.labels.push("Goofy");
-  chart.update();
+	chart.data.labels.push("Goofy");
+	chart.update();
 }
---
+++ src/types.js
@@ -0,0 +1,20 @@
+const Rarity = {
+	NONE:      0,
+	COMMON:    1,
+	RARE:      2,
+	EPIC:      3,
+	LEGENDARY: 4,
+	MYTHIC:    5
+};
+
+class Modifiers {
+	constructor(amped, marked, fortified, tacReload, accuracy, headshotRate, legRate) {
+		
+	}
+}
+
+class WeaponMods {
+	constructor(mag, stock, bolt, disruptor, hammerpoint) {
+		
+	}
+}
